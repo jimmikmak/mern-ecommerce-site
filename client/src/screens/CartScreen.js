79a98218ac80c8b3b7b370d18edbb09e9c140422
James McCarron <mccarron.j@gmail.com>
2021-06-24 16:16:17 +1000

    Added remove from cart handler function

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -12,7 +12,7 @@ import {
 } from "react-bootstrap";
 
 import Message from "../components/Message";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 import { object } from "prop-types";
 
 const CartScreen = ({ match, location, history }) => {
@@ -32,7 +32,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    console.log(object);
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
