dbfcf6ae5e56b9c9a92a8ef37cd1d96ebb40d91d
James McCarron <mccarron.j@gmail.com>
2021-06-24 16:06:29 +1000

    Finished CartScreen and added some styling adjustments

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -17,7 +17,7 @@ import { listProductDetails } from "../actions/productActions";
 // import e from "express";
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
 
