64c2fea76c234c98649ffda4579640c46a04a658
James McCarron <mccarron.j@gmail.com>
2021-07-04 12:47:09 +1000

    Created a user authentication endpoint

--- server/server.js
+++ server/server.js
@@ -3,13 +3,17 @@ import dotenv from "dotenv";
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 // Custom middleware example ->
 
@@ -23,9 +27,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
