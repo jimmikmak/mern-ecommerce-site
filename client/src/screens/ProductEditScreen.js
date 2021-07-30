a9a5c1b11d4895764bec00d6ba9033726d3e1e2b
James McCarron <mccarron.j@gmail.com>
2021-07-30 17:46:11 +1000

    Axios import in ProductEditScreen

--- client/src/screens/ProductEditScreen.js
+++ client/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
